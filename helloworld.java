6f551880bdd4eed8ece48b0b6258728a71237146
ruchir1810-ai <23h51a05c9@cmrcet.ac.in>
2026-06-03 21:59:43 +0530

    newly updated helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,5 @@
-public class HelloWorld {
-    
-    // Your program begins with a call to main()
-    public static void main(String[] args)
-    {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello Jenkins Build 16");
+class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins CI");
     }
 }
